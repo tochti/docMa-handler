5bcdf36d4233e9f6be5c68e1be27d1f83c28459d
Tim Tochtermann <tim.tochtermann@gmail.com>
2016-01-27 15:13:36 +0100

    add find funcs

--- accountingData/db.go
+++ accountingData/db.go
@@ -1,7 +1,125 @@
 package accountingData
 
-import "gopkg.in/gorp.v1"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"reflect"
+	"regexp"
+	"time"
+
+	"github.com/tochti/docMa-handler/docs"
+
+	"gopkg.in/gorp.v1"
+)
 
 func AddTables(db *gorp.DbMap) {
 	db.AddTableWithName(AccountingData{}, AccountingDataTable).SetKeys(true, "id")
+}
+
+func FindAccountingDataByDocNumbers(db *gorp.DbMap, docNumbers []docs.DocNumber) ([]AccountingData, error) {
+
+	filters := `
+		WHERE (
+			accountingData.doc_number=?
+			AND accountingData.doc_number_range=?
+		)
+	`
+	for x := 1; x < len(docNumbers); x++ {
+		q := `
+			OR(
+				accountingData.doc_number=?
+				AND accountingData.doc_number_range=?
+			)
+		`
+		filters = fmt.Sprintf("%v OR %v", filters, q)
+	}
+
+	q := Q(`
+		SELECT *
+		FROM %v as accountingData
+		%v
+	`, AccountingDataTable, filters)
+
+	flatDocNumbers := []string{}
+	for _, n := range docNumbers {
+		rang, number, err := SplitDocNumber(n.Number)
+		if err != nil {
+			return []AccountingData{}, nil
+		}
+		flatDocNumbers = append(flatDocNumbers, number, rang)
+	}
+
+	l := []AccountingData{}
+	if _, err := db.Select(&l, q, IfaceSlice(flatDocNumbers)...); err != nil {
+		return []AccountingData{}, err
+	}
+
+	return l, nil
+}
+
+func FindAccountingDataByAccountNumber(db *gorp.DbMap, accountNumber int, from time.Time, to time.Time) ([]AccountingData, error) {
+	q := Q(`
+		SELECT * 
+		FROM %v as accountingData
+		WHERE ( 
+			(doc_date BETWEEN ? AND ?) 
+			AND 
+			(credit_account=? OR debit_account=?) 
+		)
+	`, AccountingDataTable)
+
+	l := []AccountingData{}
+	if _, err := db.Select(&l, q, from, to, accountNumber, accountNumber); err != nil {
+		return []AccountingData{}, err
+	}
+
+	return l, nil
+}
+
+// Make []"any type" to []interface{}
+func IfaceSlice(slice interface{}) []interface{} {
+	s := reflect.ValueOf(slice)
+	if s.Kind() != reflect.Slice {
+		log.Fatal("InterfaceSlice() given a non-slice type")
+	}
+
+	ret := make([]interface{}, s.Len())
+
+	for i := 0; i < s.Len(); i++ {
+		ret[i] = s.Index(i).Interface()
+	}
+
+	return ret
+}
+
+func SplitDocNumber(docNumber string) (string, string, error) {
+	reStr := "([[:alpha:]]*)(\\d+)"
+	re, err := regexp.Compile(reStr)
+	if err != nil {
+		return "", "", err
+	}
+	results := re.FindStringSubmatch(docNumber)
+	l := len(results)
+	if (l != 2) && (l != 3) {
+		err := errors.New("Invalid docnumber!")
+		return "", "", err
+	}
+
+	rang := ""
+	number := ""
+
+	switch l {
+	case 2:
+		number = results[1]
+	case 3:
+		rang = results[1]
+		number = results[2]
+	}
+
+	return rang, number, nil
+}
+
+func Q(q string, p ...interface{}) string {
+	return fmt.Sprintf(q, p...)
 }
---
+++ accountingData/db_test.go
@@ -0,0 +1,142 @@
+package accountingData
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/tochti/docMa-handler/common"
+	"github.com/tochti/docMa-handler/docs"
+	"github.com/tochti/gin-gum/gumtest"
+)
+
+func Test_FindAccountingDataByDocNumbers(t *testing.T) {
+	db := common.InitTestDB(t, AddTables, AddTables)
+
+	tmpD := gumtest.SimpleNow()
+	accData := AccountingData{
+		ID:               1,
+		DocDate:          tmpD,
+		DateOfEntry:      tmpD,
+		DocNumberRange:   "DNR",
+		DocNumber:        "123",
+		PostingText:      "PT",
+		AmountPosted:     1.1,
+		DebitAccount:     1400,
+		CreditAccount:    1500,
+		TaxCode:          1,
+		CostUnit1:        "CU1",
+		CostUnit2:        "CU2",
+		AmountPostedEuro: 1.2,
+		Currency:         "EUR",
+	}
+
+	if err := db.Insert(&accData); err != nil {
+		t.Fatal(err)
+	}
+
+	docNumbers := []docs.DocNumber{
+		docs.DocNumber{
+			DocID:  1,
+			Number: "DNR123",
+		},
+	}
+	r, err := FindAccountingDataByDocNumbers(db, docNumbers)
+	if err != nil {
+		t.Fatal(r)
+	}
+
+	expect := []AccountingData{accData}
+	if len(expect) != len(r) {
+		t.Fatalf("Expect len %v was %v", len(expect), len(r))
+	}
+
+	for i, e := range expect {
+		if e.ID != r[i].ID {
+			t.Fatalf("Expect %v was %v", expect, r)
+		}
+	}
+
+}
+
+func Test_FindAccountingDataByAccountNumber(t *testing.T) {
+	db := common.InitTestDB(t, AddTables, AddTables)
+
+	tmpD := gumtest.SimpleNow()
+	accData := AccountingData{
+		ID:               1,
+		DocDate:          tmpD,
+		DateOfEntry:      tmpD,
+		DocNumberRange:   "DNR",
+		DocNumber:        "123",
+		PostingText:      "PT",
+		AmountPosted:     1.1,
+		DebitAccount:     1400,
+		CreditAccount:    1500,
+		TaxCode:          1,
+		CostUnit1:        "CU1",
+		CostUnit2:        "CU2",
+		AmountPostedEuro: 1.2,
+		Currency:         "EUR",
+	}
+
+	if err := db.Insert(&accData); err != nil {
+		t.Fatal(err)
+	}
+
+	from := gumtest.SimpleNow().Add(-48 * time.Hour)
+	to := gumtest.SimpleNow().Add(+48 * time.Hour)
+	r, err := FindAccountingDataByAccountNumber(db, 1400, from, to)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expect := []AccountingData{accData}
+	if len(expect) != len(r) {
+		t.Fatalf("Expect len %v was %v", len(expect), len(r))
+	}
+
+	for i, e := range expect {
+		if e.ID != r[i].ID {
+			t.Fatalf("Expect %v was %v", expect, r)
+		}
+	}
+
+}
+
+func Test_SplitDocNumber_OK(t *testing.T) {
+	rang, number, err := SplitDocNumber("BB123")
+	if err != nil {
+		t.Fatal("Expect nil was", err)
+	}
+	if rang != "BB" {
+		t.Fatal("Expect BB was", rang)
+	}
+	if number != "123" {
+		t.Fatal("Expect 123 was", number)
+	}
+
+	rang, number, err = SplitDocNumber("987")
+	if err != nil {
+		t.Fatal("Expect nil was", err)
+	}
+	if rang != "" {
+		t.Fatal("Expect empty string was", rang)
+	}
+	if number != "987" {
+		t.Fatal("Expect 987 was", number)
+	}
+}
+
+func Test_SplitDocNumber_Fail(t *testing.T) {
+	_, _, err := SplitDocNumber("BB")
+
+	expectErrMsg := "Invalid docnumber!"
+	if err != nil {
+		if strings.Contains(err.Error(), expectErrMsg) == false {
+			t.Fatal("Expect", expectErrMsg, "was nil")
+		}
+	} else {
+		t.Fatal("Expect", expectErrMsg, "was nil")
+	}
+}
