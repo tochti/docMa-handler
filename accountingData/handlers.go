b96cdcd51f71eb918045c28e6f74d230993a2898
Tim Tochtermann <tim.tochtermann@gmail.com>
2016-01-30 14:14:48 +0100

    add FindAllAccountingDataOfDocHandler

--- accountingData/handlers.go
+++ accountingData/handlers.go
@@ -1,9 +1,12 @@
 package accountingData
 
 import (
+	"database/sql"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tochti/docMa-handler/docs"
 	"github.com/tochti/docMa-handler/valid"
 	"github.com/tochti/gin-gum/gumrest"
 	"gopkg.in/gorp.v1"
@@ -28,4 +31,88 @@ func CreateAccountingDataHandler(ginCtx *gin.Context, db *gorp.DbMap) {
 
 	ginCtx.JSON(http.StatusCreated, accountingData)
 
+}
+
+func FindAllAccountingDataOfDocHandler(ginCtx *gin.Context, db *gorp.DbMap) {
+	id, err := ReadDocID(ginCtx)
+	if err != nil {
+		return
+	}
+
+	docNumbers := []docs.DocNumber{}
+	_, err = db.Select(&docNumbers, Q("SELECT * FROM %v WHERE doc_id=?", docs.DocNumbersTable), id)
+	if err != nil {
+		gumrest.ErrorResponse(ginCtx, http.StatusBadRequest, err)
+		return
+	}
+
+	var r1 []AccountingData
+	if len(docNumbers) > 0 {
+		var err error
+		r1, err = FindAccountingDataByDocNumbers(db, docNumbers)
+		if err != nil {
+			gumrest.ErrorResponse(ginCtx, http.StatusBadRequest, err)
+			return
+		}
+	}
+
+	accountData := docs.DocAccountData{}
+	err = db.SelectOne(&accountData, Q("SELECT * FROM %v WHERE doc_id=?", docs.DocAccountDataTable), id)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			gumrest.ErrorResponse(ginCtx, http.StatusBadRequest, err)
+			return
+		}
+	}
+
+	var r2 []AccountingData
+	if err != sql.ErrNoRows {
+		var err error
+		r2, err = FindAccountingDataByAccountNumber(
+			db,
+			accountData.AccountNumber,
+			accountData.PeriodFrom,
+			accountData.PeriodTo,
+		)
+		if err != nil {
+			gumrest.ErrorResponse(ginCtx, http.StatusBadRequest, err)
+			return
+		}
+	}
+
+	r := mergeAccountingData(r1, r2)
+
+	ginCtx.JSON(http.StatusOK, r)
+}
+
+func mergeAccountingData(a1, a2 []AccountingData) []AccountingData {
+	ids := map[int64]bool{}
+	r := a1
+
+	for _, e := range a1 {
+		ids[e.ID] = true
+	}
+
+	for _, e := range a2 {
+		if _, ok := ids[e.ID]; !ok {
+			r = append(r, e)
+		}
+	}
+
+	return r
+}
+
+func ReadDocID(c *gin.Context) (int64, error) {
+	tmp := c.Params.ByName("id")
+	labelID, err := strconv.ParseInt(tmp, 10, 64)
+	if err != nil {
+		gumrest.ErrorResponse(
+			c,
+			http.StatusBadRequest,
+			err,
+		)
+		return -1, err
+	}
+
+	return labelID, nil
 }
--- accountingData/handlers_test.go
+++ accountingData/handlers_test.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tochti/docMa-handler/common"
+	"github.com/tochti/docMa-handler/docs"
 	"github.com/tochti/gin-gum/gumtest"
 	"github.com/tochti/gin-gum/gumwrap"
 	"gopkg.in/gorp.v1"
@@ -47,11 +49,9 @@ func Test_CreateAccountingDataHandler(t *testing.T) {
 	}
 }
 
-func Test_FindAccountingDataHandler(t *testing.T) {
-	db := initDB(t)
-
+func Test_MergeAccountingData(t *testing.T) {
 	tmpD := gumtest.SimpleNow()
-	accountingData := AccountingData{
+	accData := AccountingData{
 		ID:               1,
 		DocDate:          tmpD,
 		DateOfEntry:      tmpD,
@@ -68,15 +68,193 @@ func Test_FindAccountingDataHandler(t *testing.T) {
 		Currency:         "EUR",
 	}
 
-	body, err := json.Marshal(accountingData)
-	if err != nil {
+	a1 := []AccountingData{accData}
+	a2 := []AccountingData{accData}
+
+	r := mergeAccountingData(a1, a2)
+	if len(r) != 1 {
+		t.Fatal("Expect len 1 was", len(r))
+	}
+
+	if r[0].ID != 1 {
+		t.Fatal("Expect 1 was", r[0].ID)
+	}
+}
+
+func Test_FindAllAccountingDataOfDocHandler(t *testing.T) {
+	db := common.InitTestDB(t, AddTables, docs.AddTables)
+
+	docNumber := docs.DocNumber{
+		DocID:  1,
+		Number: "DNR123",
+	}
+
+	tmpD := gumtest.SimpleNow()
+	accountData := docs.DocAccountData{
+		DocID:         1,
+		PeriodFrom:    tmpD,
+		PeriodTo:      tmpD,
+		AccountNumber: 1400,
+	}
+
+	accData1 := AccountingData{
+		ID:               1,
+		DocDate:          tmpD,
+		DateOfEntry:      tmpD,
+		DocNumberRange:   "DNR",
+		DocNumber:        "123",
+		PostingText:      "PT",
+		AmountPosted:     1.1,
+		DebitAccount:     1400,
+		CreditAccount:    1500,
+		TaxCode:          1,
+		CostUnit1:        "CU1",
+		CostUnit2:        "CU2",
+		AmountPostedEuro: 1.2,
+		Currency:         "EUR",
+	}
+
+	accData2 := AccountingData{
+		ID:               2,
+		DocDate:          tmpD,
+		DateOfEntry:      tmpD,
+		DocNumberRange:   "X",
+		DocNumber:        "1",
+		PostingText:      "PT",
+		AmountPosted:     1.1,
+		DebitAccount:     1400,
+		CreditAccount:    1500,
+		TaxCode:          1,
+		CostUnit1:        "CU1",
+		CostUnit2:        "CU2",
+		AmountPostedEuro: 1.2,
+		Currency:         "EUR",
+	}
+
+	if err := db.Insert(&accountData, &docNumber, &accData1, &accData2); err != nil {
 		t.Fatal(err)
 	}
 
 	r := gin.New()
-	r.POST("/", gumwrap.Gorp(CreateAccountingDataHandler, db))
-	resp := gumtest.NewRouter(r).ServeHTTP("POST", "/", string(body))
-	expectResp := gumtest.JSONResponse{http.StatusCreated, accountingData}
+	r.GET("/:id", gumwrap.Gorp(FindAllAccountingDataOfDocHandler, db))
+	resp := gumtest.NewRouter(r).ServeHTTP("GET", "/1", "")
+
+	expectResp := gumtest.JSONResponse{
+		http.StatusOK,
+		[]AccountingData{
+			accData1,
+			accData2,
+		},
+	}
+	if err := gumtest.EqualJSONResponse(expectResp, resp); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_FindAllAccountingDataOfDocHandler_NoDocNumbers(t *testing.T) {
+	db := common.InitTestDB(t, AddTables, docs.AddTables)
+
+	tmpD := gumtest.SimpleNow()
+	accountData := docs.DocAccountData{
+		DocID:         1,
+		PeriodFrom:    tmpD,
+		PeriodTo:      tmpD,
+		AccountNumber: 1400,
+	}
+
+	accData1 := AccountingData{
+		ID:               1,
+		DocDate:          tmpD,
+		DateOfEntry:      tmpD,
+		DocNumberRange:   "DNR",
+		DocNumber:        "123",
+		PostingText:      "PT",
+		AmountPosted:     1.1,
+		DebitAccount:     1400,
+		CreditAccount:    1500,
+		TaxCode:          1,
+		CostUnit1:        "CU1",
+		CostUnit2:        "CU2",
+		AmountPostedEuro: 1.2,
+		Currency:         "EUR",
+	}
+
+	accData2 := AccountingData{
+		ID:               2,
+		DocDate:          tmpD.Add(48 * time.Hour),
+		DateOfEntry:      tmpD.Add(48 * time.Hour),
+		DocNumberRange:   "DNR",
+		DocNumber:        "123",
+		PostingText:      "PT",
+		AmountPosted:     1.1,
+		DebitAccount:     1400,
+		CreditAccount:    1500,
+		TaxCode:          1,
+		CostUnit1:        "CU1",
+		CostUnit2:        "CU2",
+		AmountPostedEuro: 1.2,
+		Currency:         "EUR",
+	}
+
+	if err := db.Insert(&accountData, &accData1, &accData2); err != nil {
+		t.Fatal(err)
+	}
+
+	r := gin.New()
+	r.GET("/:id", gumwrap.Gorp(FindAllAccountingDataOfDocHandler, db))
+	resp := gumtest.NewRouter(r).ServeHTTP("GET", "/1", "")
+
+	expectResp := gumtest.JSONResponse{
+		http.StatusOK,
+		[]AccountingData{
+			accData1,
+		},
+	}
+	if err := gumtest.EqualJSONResponse(expectResp, resp); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_FindAllAccountingDataOfDocHandler_NoAccountData(t *testing.T) {
+	db := common.InitTestDB(t, AddTables, docs.AddTables)
+
+	docNumber := docs.DocNumber{
+		DocID:  1,
+		Number: "DNR123",
+	}
+
+	tmpD := gumtest.SimpleNow()
+	accData1 := AccountingData{
+		ID:               1,
+		DocDate:          tmpD,
+		DateOfEntry:      tmpD,
+		DocNumberRange:   "DNR",
+		DocNumber:        "123",
+		PostingText:      "PT",
+		AmountPosted:     1.1,
+		DebitAccount:     1400,
+		CreditAccount:    1500,
+		TaxCode:          1,
+		CostUnit1:        "CU1",
+		CostUnit2:        "CU2",
+		AmountPostedEuro: 1.2,
+		Currency:         "EUR",
+	}
+
+	if err := db.Insert(&docNumber, &accData1); err != nil {
+		t.Fatal(err)
+	}
+
+	r := gin.New()
+	r.GET("/:id", gumwrap.Gorp(FindAllAccountingDataOfDocHandler, db))
+	resp := gumtest.NewRouter(r).ServeHTTP("GET", "/1", "")
+
+	expectResp := gumtest.JSONResponse{
+		http.StatusOK,
+		[]AccountingData{
+			accData1,
+		},
+	}
 	if err := gumtest.EqualJSONResponse(expectResp, resp); err != nil {
 		t.Fatal(err)
 	}
